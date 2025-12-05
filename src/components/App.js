3d6969efb5aec3e35616c80f74e6cc853b48b5b3
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-12-05 11:56:14 +0530

    submit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+      <RenderParagraph />
     </div>
   );
 }
 
+function RenderParagraph() {
+  const [hide, setHide] = useState(false);
+
+  const para = (
+    <p id="para">
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so
+      happy
+    </p>
+  );
+
+  return (
+    <>
+      <button id="click" onClick={() => setHide(true)}>
+        click
+      </button>
+      {hide && para}
+    </>
+  );
+}
 
 export default App;
